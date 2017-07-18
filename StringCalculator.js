4eba6e4bf3ef1e1d609cd7fa1db90070a8a12ecc
Aliaksandr Yeudakou <thevdakov@gmail.com>
2017-07-18 18:46:10 +0300

    simple implementation.

---
+++ StringCalculator.js
@@ -0,0 +1,34 @@
+/**
+ * Develop a comprehensive set of methods for executing the functions of the calculator
+ * over the numbers passed in the string representation. Methods must allow work in both
+ * integer and real format.
+ */
+var stringCalculator;
+stringCalculator = function () {
+
+    return {
+        addition: function(a, b) {
+            var param_1 = parseFloat(a),
+                param_2 = parseFloat(b);
+            return param_1 + param_2;
+        },
+
+        subtraction: function(a, b) {
+            var param_1 = parseFloat(a),
+                param_2 = parseFloat(b);
+            return param_1 - param_2;
+        },
+
+        multiplication: function(a, b){
+            var param_1 = parseFloat(a),
+                param_2 = parseFloat(b);
+            return param_1 * param_2;
+        },
+
+        division: function(a, b) {
+            var param_1 = parseFloat(a),
+                param_2 = parseFloat(b);
+            return param_1 / param_2;
+        }
+    };
+}();
